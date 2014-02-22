703ccba8e607237d0686cd48db6423375111ac6e
Marco Chiang <marcochiangtw@gmail.com>
2014-02-21 22:12:44 -0500

    first init commit for Gestice iOS App

---
+++ ios_justice/Gestice/Gestice/LGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LGAppDelegate.h
+//  Gestice
+//
+//  Created by Marco Chiang on 2/21/14.
+//  Copyright (c) 2014 Leaf, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios_justice/Gestice/Gestice/LGViewController.h
@@ -0,0 +1,13 @@
+//
+//  LGViewController.h
+//  Gestice
+//
+//  Created by Marco Chiang on 2/21/14.
+//  Copyright (c) 2014 Leaf, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LGViewController : UIViewController
+
+@end
